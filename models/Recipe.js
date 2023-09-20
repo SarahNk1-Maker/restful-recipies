25d05f1fdafc66d4982a3de0acfa618258c34b6f
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-09-20 12:49:53 -0400

    created recipe & user models, plus index

---
+++ models/Recipe.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    chef: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Recipe',
+  }
+);
+
+module.exports = Recipe;
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Recipe = require('./Recipe');
+
+User.hasmany(Recipe, {
+    forigenKey: 'user-id',
+    onDelete: 'CASCADE'
+})
+
+Recipe.belongsTo(User, {
+    forigenKey: 'user-id'
+});
+
+module.exports - { User, Recipe }
