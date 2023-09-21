e1acf9e86d2ce0021e29b5d2c11f6fda9eaa2e01
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-09-21 16:19:22 -0400

    squashed them all

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./recipeRoutes');
+const recipeRoutes = require('./recipeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
