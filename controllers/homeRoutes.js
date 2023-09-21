dc324c66831ccc6631213072131abc92f84959e3
SarahNk1-Maker <sarahnaveed216@gmail.com>
2023-09-21 14:07:07 -0400

    added get route to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Recipe, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     // Get all recipes and JOIN with user data
     const recipeData = await Recipe.findAll({
@@ -27,7 +27,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+// Route to render the recipe page based on ID
+router.get('/recipe/:id',withAuth, async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+
+    // Query the database to find the recipe by its ID
+    const recipe = await Recipe.findById(recipeId);
+
+    if (!recipe) {
+      return res.status(404).send('Recipe not found');
+    }
+
+    // Render the Handlebars template with the retrieved recipe data
+    res.render('recipe', { recipe }); 
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/login',withAuth, (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
       res.redirect('/profile');
