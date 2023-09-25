1d858097312fb7b37a3bc700333f1c63947006ff
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-25 19:55:47 -0400

    upload now works but doesnt save, added styles

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,14 +1,18 @@
 var myWidget = cloudinary.createUploadWidget({
-    cloudName: 'cloud',
-    uploadPreset: 'upload',
-    folder: 'upload'
+    cloudName: 'hzxyensd5',
+    uploadPreset: 'aoh4fpwm',
+    folder: 'upload',
+    theme: "orange"
 },
     (error, result) => {
         if (!error && result && result.event === "success") {
-            console.log('Done! Here is the image info: ', result.info);
+            console.log("Done! Here is the image info: ", result.info);
+            document
+                .getElementById("uploadedimage")
+                .setAttribute("src", result.info.secure_url);
         }
     }
-)
+);
 
 document.getElementById("upload_widget").addEventListener("click", function () {
     myWidget.open();
