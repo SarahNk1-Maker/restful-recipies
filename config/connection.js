f7425be6cab7abccb9f31735c57dc7ec19d1160e
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-24 23:36:37 -0400

    added couldinary package and code to implement upload feature

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
+});
 
 let sequelize;
 
@@ -19,3 +27,4 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
+exports.cloudinary = cloudinary;
---
+++ public/js/upload.js
@@ -0,0 +1,17 @@
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'cloud',
+    uploadPreset: 'upload',
+    folder: 'upload'
+},
+    (error, result) => {
+        if (!error && result && result.event === "success") {
+            console.log('Done! Here is the image info: ', result.info);
+        }
+    }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function () {
+    myWidget.open();
+},
+    false
+);
